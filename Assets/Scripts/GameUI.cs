e0daeeb27f64216880320d9ae609f27ee5e43bed
mishrazb@hotmail.com <mishrazb@hotmail.com>
2025-02-11 22:24:57 -0500

    Added GameUI to manage undo moves

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -10,8 +10,27 @@ public class GameUI : MonoBehaviour
     public void OnResign(){
         Debug.Log("Resign");
     }
-    public void OnUndoMove(){
-        Debug.Log("Undo Move");
+    public void OnUndoMove()
+    {
+        // Check with the GameManager if there are moves to undo.
+        if (GameManager.Instance.HasMoveRecords() && GameManager.Instance.MaxUndoMoves>0)
+        {
+            bool success = GameManager.Instance.UndoLastMove();
+            GameManager.Instance.MaxUndoMoves--;
+            GameManager.Instance.UndoMoveBtnText.text = GameManager.Instance.MaxUndoMoves.ToString();
+            if (success)
+            {
+                Debug.Log("Undo successful.");
+            }
+            else
+            {
+                Debug.LogWarning("Undo failed: unable to revert the last move.");
+            }
+        }
+        else
+        {
+            Debug.Log("No moves to undo.");
+        }
     }
     public void OnRestartGame(){
         Debug.Log("Restart Game");
